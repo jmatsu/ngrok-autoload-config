27b7e9a8757cfbc884dd0ce5329ffca9988af21d
Jumpei Matsuda <jmatsu.drm@gmail.com>
2019-03-24 03:08:41 +0900

    Set BATS_ROOT on CI

--- bin/exec_all_bats.bash
+++ bin/exec_all_bats.bash
@@ -6,8 +6,6 @@ set -o pipefail
 cd test
 
 export PATH="$PWD/bin:$PATH"
-export PATH="$PWD/lib/bats/bin:$PATH"
-export PATH="$PWD/lib/bats/libexec/bats-core:$PATH"
 
 while read test_file; do
   $test_file
